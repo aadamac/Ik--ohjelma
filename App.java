cec3b784607cb140e39416cd3baa70e5c379dd75
aadamac <169080987+aadamac@users.noreply.github.com>
2024-09-16 13:04:44 +0300

    Update App.java
    Vaidoin ikää

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int ika = 45;
+        int ika = 20;
 
         if (ika >= 0 && ika < 18) {
             System.out.println("Olet alaikäinen");
